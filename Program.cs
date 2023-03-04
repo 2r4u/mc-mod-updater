d6f436aeb020c49dca019b2795ccbb3f2abb1f5e
Anthony Olebe <2r4u666@gmail.com>
2023-03-04 13:58:44 -0800

    added json deserialzation + Mod class
    pog

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 using System.IO;
 using System.Collections;
-
+using System;
 
 HttpClient client = new HttpClient();
 client.DefaultRequestHeaders.Accept.Clear();
@@ -20,7 +20,7 @@ String version = Console.ReadLine();
 String mcPath = "..\\..\\..\\..\\..\\..\\AppData\\Roaming\\.minecraft\\mods";
     
 string[] files = Directory.GetFiles(@mcPath);
-ArrayList mods = new ArrayList();
+List<Mod> mods = new List<Mod>();
 ArrayList fnames = new ArrayList();
 Console.WriteLine("These are the files in your mods folder:");
 foreach (string file in files) {
@@ -32,11 +32,25 @@ if (yn(Console.ReadLine()))
 {
     foreach (String file in fnames)
     {
-        await updateMods(client, file, version);
+        await updateMods(client, file, version,mods);
     }
-    Console.WriteLine("These are the file names of the mods that will be downloaded:");
-    var jsonfile = JsonSerializer.Deserialize<JsonElement>(File.ReadAllText(@"..\\..\\..\\data.json"));
-    Console.WriteLine(FromJsonElement(jsonfile));
+    clean(mods);
+    Console.WriteLine("==================================================");
+    Console.WriteLine("These are the mods that will be downloaded:");
+    Thread.Sleep(3000);
+    for (int i=0;i<mods.Count; i++)
+    {
+        Mod m = mods[i];
+        Console.WriteLine(m.Name+": "+m.Description);
+    }
+    Thread.Sleep(3000);
+    Console.WriteLine("Some of these mods may not be correct. Please enter the number of the one you wish to remove.");
+    while (true)
+        //get user to narrow down mods until all are correct
+    {
+        Console.WriteLine((i + 1) + ". " +);
+        int mn = Int32.Parse(Console.ReadLine());
+    }
 }
 else
 {
@@ -44,7 +58,7 @@ else
 }
 static async Task eraseJson(string path)
 {
-    File.WriteAllText(@path, "{\n\t\n}");
+    File.WriteAllText(@path, String.Empty);
 }
 static (string? name, String author) FromJsonElement(JsonElement jsonElement)
 {
@@ -52,19 +66,30 @@ static (string? name, String author) FromJsonElement(JsonElement jsonElement)
     var author = jsonElement.GetProperty("hits").GetProperty("author").GetString();
     return (name, author);
 }
-
-static async Task updateMods(HttpClient client, String file, String version) 
-{   
-    var json = await client.GetStringAsync("https://api.modrinth.com/v2/search?limit=1&query="+GetUntilOrEmpty(1,file,"-")+"&index=downloads&facets=[[\"categories:fabric\"],[\"versions:" + version + "\"]]");
-    /*.Add(JsonSerializer.Deserialize<Mod>(json));
-    using JsonDocument doc = JsonDocument.Parse(json);
+static async Task updateMods(HttpClient client, String file, String version, List<Mod> list)
+{
+    var json = await client.GetStringAsync("https://api.modrinth.com/v2/search?limit=1&query=" + GetUntilOrEmpty(1, file, "-") + "&index=downloads&facets=[[\"categories:fabric\"],[\"versions:" + version + "\"]]");
+    /*File.AppendAllText(@"..\\..\\..\\data.json", json);*/
+    Console.WriteLine(GetUntilOrEmpty(1, file, "-"));
+    try
+    {
+        var f = JsonSerializer.Deserialize<Root>(json);
+        Console.WriteLine(f.hits[0].title);
+        list.Add(new Mod(f.hits[0].title, f.hits[0].description, f.hits[0].versions));
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+        Console.WriteLine("This mod might not be on Modrinth or updated to this version.");
+    }
+    /*using JsonDocument doc = JsonDocument.Parse(json);
     JsonElement root = doc.RootElement;
     String js = ""1""root[0].Ge;*/
-    Console.WriteLine(GetUntilOrEmpty(1, file, "-"));
+    
     /*List<string> lines = File.ReadAllLines(@"..\\..\\..\\data.json").ToList<string>();
     lines.Insert(lines.Capacity - 2, js);*/
-   /* File.WriteAllLines(@"..\\..\\..\\data.json", lines);*/
-    File.AppendAllText(@"..\\..\\..\\data.json", json);
+    /* File.WriteAllLines(@"..\\..\\..\\data.json", lines);*/
+    
 
 
 
@@ -116,17 +141,77 @@ bool yn(String str)
     }
     return false;
 }
-class Mod
+void clean(List<Mod> list)
 {
-    public string id {  get; set; }
-    public string title { get; set; }
-    public string description { get; set; }
-
-    public Mod(string id, string title, string description)
+    List<string> names = new List<string>();
+    
+    if(list != null) 
     {
-        this.id = id;
-        this.title = title;
-        this.description = description;
+
+        foreach (Mod mod in list.ToList())
+        {
+            if (names.Contains(mod.Name))
+            {
+               list.Remove(mod);
+            }
+            else
+            {
+                names.Add(mod.Name);
+            }
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("bruh");
     }
 }
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+public class Mod
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<string> Versions { get; set; }
+    public Mod(string name, string description, List<string> versions)
+    {
+        Name = name;
+        Description = description;
+        Versions = versions;
+    }
+    
+}
+public class Hit
+{
+    public string project_id { get; set; }
+    public string project_type { get; set; }
+    public string slug { get; set; }
+    public string author { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public List<string> categories { get; set; }
+    public List<string> display_categories { get; set; }
+    public List<string> versions { get; set; }
+    public int downloads { get; set; }
+    public int follows { get; set; }
+    public string icon_url { get; set; }
+    public DateTime date_created { get; set; }
+    public DateTime date_modified { get; set; }
+    public string latest_version { get; set; }
+    public string license { get; set; }
+    public string client_side { get; set; }
+    public string server_side { get; set; }
+    public List<object> gallery { get; set; }
+    public object featured_gallery { get; set; }
+    public int color { get; set; }
+}
+
+public class Root
+{
+    public List<Hit> hits { get; set; }
+    public int offset { get; set; }
+    public int limit { get; set; }
+    public int total_hits { get; set; }
+}
+
+
 
